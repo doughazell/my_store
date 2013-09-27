0c397b0412ee863ead130e83917875c309309df3
Doug Hazell <doughazell@gmail.com>
2013-09-27 12:55:23 +0100

    Initial import of working BSC eCommerce site.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyStore::Application.config.secret_token = '9c8930935018dfd1c10624eeb899127a1040ac7375718bd842719ebdbebfec68ab4dde73b99f5840fea127efc34805ef129b35aac86c145372b18ee688ca4043'
